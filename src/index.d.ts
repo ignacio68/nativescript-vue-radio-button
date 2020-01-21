833bfd6cd01ddc85b27cf046eaea532c6fba84b4
Ignacio <buscemi@ya.com>
2020-01-21 12:22:00 +0100

    first commit

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './vue-radio-button.common';
+export declare class VueRadioButton extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
---
+++ src/vue-radio-button.ios.ts
@@ -0,0 +1,5 @@
+import { Common } from './vue-radio-button.common';
+
+export class VueRadioButton extends Common {
+
+}
